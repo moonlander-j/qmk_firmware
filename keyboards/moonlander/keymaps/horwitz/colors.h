3ccf3dc0b25fc37e55af3e65869b1e64c4c14f36
Jeremy Horwitz <jhmail@gmail.com>
2023-10-19 10:57:14 -0700

    create (and use) colors.h (HSV_DAVYS_GRAY, ..., HSV_CARDINAL)

---
+++ keyboards/moonlander/keymaps/horwitz/colors.h
@@ -0,0 +1,15 @@
+#define HSV_DAVYS_GRAY     0,   0,  85 // #555555 (white, 1/3 value)
+#define HSV_GRAY40         0,   0, 102 // #666666 (white, 40% value)
+#define HSV_DARK_GRAY      0,   0, 170 // #AAAAAA (white, 2/3 value)
+#define HSV_MAROON         0, 255, 128 // #800000
+#define HSV_SFG_ORANGE    11, 226, 254 // #FE5A1D
+#define HSV_VERM50        11, 255, 128 // #802000 (vermillion, 50% value)
+#define HSV_VERM75        11, 255, 192 // #BF3000 (vermillion, 75% value)
+#define HSV_VERMILLION    11, 255, 255 // #FF4000 (halfway between red and orange)
+#define HSV_ALLIGATOR     35, 255, 135 // #877100
+#define HSV_ZELDA_CART    43,  90,  87 // TODO update color
+#define HSV_OLIVE         43, 255, 128 // #808000
+#define HSV_GREEN50       85, 255, 128 // #008000 (green, 50% value)
+#define HSV_PACIFIC_BLUE 138, 219, 205 // #1DA0CD
+#define HSV_PRINCE_PURP  194, 126, 196 // TODO update color
+#define HSV_CARDINAL     248, 216, 196 // #C41E3A
