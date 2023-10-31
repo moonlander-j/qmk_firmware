b6cdbf19e60c6d30d8e3d82b313fa109f49ff92e
Jeremy Horwitz <jhmail@gmail.com>
2023-10-31 12:14:02 -0700

    expand (WIN|MAC)_SYM

--- keyboards/moonlander/keymaps/horwitz/colors.h
+++ keyboards/moonlander/keymaps/horwitz/colors.h
@@ -9,7 +9,14 @@
 #define HSV_ALLIGATOR     35, 255, 135 // #877100
 #define HSV_ZELDA_CART    43,  90,  87 // TODO update color
 #define HSV_OLIVE         43, 255, 128 // #808000
+//#define HSV_MINT_GREEN    85, 100, 251 // #98FB98
+//#define HSV_LIGHT_GREEN   85, 101, 238 // #90EE90
 #define HSV_GREEN50       85, 255, 128 // #008000 (green, 50% value)
+//#define HSV_MONEY_GREEN  106, 224, 240 // #118C4F
+//#define HSV_DARK_GREEN   113, 244,  48 // #023020
+#define HSV_LIGHT_BLUE   138,  63, 230 // #ADD8E6
 #define HSV_PACIFIC_BLUE 138, 219, 205 // #1DA0CD
 #define HSV_PRINCE_PURP  194, 126, 196 // TODO update color
+#define HSV_LILAC        213,  48, 200 // #C8A2C8
 #define HSV_CARDINAL     248, 216, 196 // #C41E3A
+//#define HSV_LIGHT_PINK   250,  73, 255 // #FFB6C1
