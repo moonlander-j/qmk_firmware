b527f961ec33ee5cf48fae3f1912e9546effaae0
Jeremy Horwitz <jhmail@gmail.com>
2023-12-30 23:43:09 -0800

    colors.h updates from qmkTools ((re)derive HSV from RGB when an RGB source is given)

--- keyboards/moonlander/keymaps/horwitz/colors.h
+++ keyboards/moonlander/keymaps/horwitz/colors.h
@@ -2,21 +2,21 @@
 #define HSV_GRAY40         0,   0, 102 // #666666 (white, 40% value)
 #define HSV_DARK_GRAY      0,   0, 170 // #AAAAAA (white, 2/3 value)
 #define HSV_MAROON         0, 255, 128 // #800000
-#define HSV_SFG_ORANGE    11, 226, 254 // #FE5A1D
-#define HSV_VERM50        11, 255, 128 // #802000 (vermillion, 50% value)
-#define HSV_VERM75        11, 255, 192 // #BF3000 (vermillion, 75% value)
-#define HSV_VERMILLION    11, 255, 255 // #FF4000 (halfway between red and orange)
-#define HSV_ALLIGATOR     35, 255, 135 // #877100
-#define HSV_ZELDA_CART    43,  90,  87 // TODO update color
-#define HSV_OLIVE         43, 255, 128 // #808000
-//#define HSV_MINT_GREEN    85, 100, 251 // #98FB98
+#define HSV_VERM50        11, 255, 128 // #802100 (original: #802000 (vermillion, 50% value))
+#define HSV_VERM75        11, 255, 191 // #BF3100 (original: #BF3000 (vermillion, 75% value))
+#define HSV_VERMILLION    11, 255, 255 // #FF4200 (original: #FF4000 (halfway between red and orange))
+#define HSV_SFG_ORANGE    12, 226, 254 // #FE5C1D (original: #FE5A1D)
+#define HSV_ALLIGATOR     36, 255, 135 // #877200 (original: #877100)
+#define HSV_ZELDA_CART    43,  90,  87 // #575738 (TODO update color)
+#define HSV_OLIVE         43, 255, 128 // #7E8000 (original: #808000)
 //#define HSV_LIGHT_GREEN   85, 101, 238 // #90EE90
+//#define HSV_MINT_GREEN    85, 101, 251 // #98FB98 (original: #98FB98)
 #define HSV_GREEN50       85, 255, 128 // #008000 (green, 50% value)
-//#define HSV_MONEY_GREEN  106, 224, 240 // #118C4F
+//#define HSV_MONEY_GREEN  106, 224, 140 // #118C4E (original: #118C4F)
 //#define HSV_DARK_GREEN   113, 244,  48 // #023020
 #define HSV_LIGHT_BLUE   138,  63, 230 // #ADD8E6
-#define HSV_PACIFIC_BLUE 138, 219, 205 // #1DA0CD
-#define HSV_PRINCE_PURP  194, 126, 196 // TODO update color
+#define HSV_PACIFIC_BLUE 138, 219, 205 // #1DA2CD (original: #1DA0CD)
+#define HSV_PRINCE_PURP  194, 126, 196 // #9A63C4 (TODO update color)
 #define HSV_LILAC        213,  48, 200 // #C8A2C8
-#define HSV_CARDINAL     248, 216, 196 // #C41E3A
-//#define HSV_LIGHT_PINK   250,  73, 255 // #FFB6C1
+#define HSV_CARDINAL     248, 216, 196 // #C41E39 (original: #C41E3A)
+//#define HSV_LIGHT_PINK   249,  73, 255 // #FFB6C0 (original: #FFB6C1)
