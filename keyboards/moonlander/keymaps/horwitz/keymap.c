51085dcebe4c3e27eec1f26342932764d2a74054
Jeremy Horwitz <jhmail@gmail.com>
2024-07-10 00:04:18 -0700

    _QWERTY: update 8 thumb keys to match _WIN_SYM

--- keyboards/moonlander/keymaps/horwitz/keymap.c
+++ keyboards/moonlander/keymaps/horwitz/keymap.c
@@ -624,8 +624,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_BSLS,                 XXXXXXX,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINUS,
         KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_LBRC,                 KC_RBRC,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,
         KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_UP,          KC_SLASH,
-        KC_LCTL,        KC_LGUI,        KC_LALT,        XXXXXXX,        XXXXXXX,                        _______,                 _______,                        XXXXXXX,        KC_RCTL,        KC_LEFT,        KC_DOWN,        KC_RIGHT,
-                                                                        _______,        _______,        _______,                 _______,        _______,        _______
+        KC_LCTL,        KC_LGUI,        KC_LALT,        XXXXXXX,        XXXXXXX,                        KC_LGUI,                 KC_CAPS,                        XXXXXXX,        KC_RCTL,        KC_LEFT,        KC_DOWN,        KC_RIGHT,
+                                                                        KC_ENTER,       KC_LALT,        MO(_ACCENT),             KC_DELETE,      KC_BSPC,        KC_SPACE
     )
     // clang-format on
 };
