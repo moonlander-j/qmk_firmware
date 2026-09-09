82fad950a564059dad45b20bfe1cf05ac5a89625
Jeremy Horwitz <jhmail@gmail.com>
2026-09-09 01:55:50 -0700

    clang extra files (from earlier commit)

---
+++ keyboards/moonlander/keymaps/horwitz/run-clang-tidy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Run clang-tidy on the horwitz keymap.
+# Must be run from the repo root.
+#
+# Prerequisites:
+#   1. Install clang-tidy:  brew install llvm
+#                           then add /opt/homebrew/opt/llvm/bin to PATH
+#   2. Generate a compilation database from a successful QMK build:
+#        qmk generate-compilation-database -kb moonlander -km horwitz
+#      This produces compile_commands.json in the repo root.
+#
+# clang-tidy reads .clang-tidy from the source file's directory for check config.
+set -e
+clang-tidy \
+    -p . \
+    keyboards/moonlander/keymaps/horwitz/keymap.c
